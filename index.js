8eeab2ca1f42f47e66fd1f784bf1d73425d377b9
Juan M <juanmegiasmarkelov@gmail.com>
2026-03-26 16:56:48 +0100

    Proyecto Final: Tienda de ropa con CRUD y SSR

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.use(methodOverride('_method'));
 
 app.use('/', require('./routes/productRoutes'));
 
+app.get('/', (req, res) => {
+    res.redirect('/products');
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Servidor en http://localhost:${PORT}/products`));
